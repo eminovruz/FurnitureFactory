efcecc82eaa23bb97bc08b9a041564f78f3fce27
Novr_wz05 <Novr_wz05@STHQ0124-07>
2022-11-23 08:45:54 +0400

    Add project files.

---
+++ DesignPatterns-3/Program.cs
@@ -0,0 +1,42 @@
+﻿public interface IPayment
+{
+    public void Pay();
+}
+
+public class Cash : IPayment
+{
+    public void Pay()
+    {
+        // Paying with cash...
+    }
+}
+
+public class CCard : IPayment
+{
+    public void Pay()
+    {
+        // Paying with bank card
+    }
+}
+
+
+public class Taxi
+{
+    IPayment? _payment;
+
+    public void GetPayment()
+    {
+        _payment.Pay();
+    }
+
+}
+
+public class Bolt : Taxi
+{
+
+}
+
+public class Uber : Taxi
+{
+
+}
